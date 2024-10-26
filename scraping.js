bf889bd31044cc1377daed95f5d2f3f743876812
Jonathas Vinicius <jonathas.vinicius@ufpe.br>
2024-10-26 11:32:50 -0300

    refactor: atualizar script de scraping para converter MP3 em MIDI e adicionar manipulação de elementos da página

--- scraping.js
+++ scraping.js
@@ -4,26 +4,47 @@ import path from 'path';
 
 (async () => {
   const browser = await puppeteer.launch({
-    headless: true, // Defina como false para ver o navegador em ação (útil para depuração)
+    headless: false, // Defina como false para ver o navegador em ação (útil para depuração)
     args: ['--no-sandbox', '--disable-setuid-sandbox']
   });
   const page = await browser.newPage();
 
-  await page.goto('https://www.google.com');
+  await page.goto('https://converter.app/mp3-to-midi/');
 
-  await page.waitForSelector('#APjFqb');
+  await page.waitForSelector('#form-element');
 
-  await page.type('#APjFqb', 'ping');
-  await page.keyboard.press('Enter');
+  const filePath = path.resolve(process.cwd(), 'famintø.mp3');
 
-  await page.waitForSelector('#search');
+  const inputUploadHandle = await page.$('input[type=file]');
+  await inputUploadHandle.uploadFile(filePath);
 
-  const screenshotsDir = path.resolve(process.cwd(), 'screenshots');
-  if (!fs.existsSync(screenshotsDir)) {
-    fs.mkdirSync(screenshotsDir);
+  await new Promise(resolve => setTimeout(resolve, 5000));
+
+  const closeAdButton = await page.$('.close-ad-button-selector')
+  if (closeAdButton) {
+    await closeAdButton.click();
+    await new Promise(resolve => setTimeout(resolve, 1000)); 
   }
 
-  await page.screenshot({ path: path.join(screenshotsDir, 'google_search.png') });
+  await page.screenshot({ path: 'before_click.png' });
+
+  const submitButton = await page.$('button[type=submit]');
+  await submitButton.click();
+
+  await page.waitForNavigation({ waitUntil: 'networkidle0' });
+
+  await page.waitForSelector('#text', { visible: true });
+
+  const downloadButton = await page.waitForSelector('.btn', { visible: true });
+
+  await downloadButton.click();
+
+  const midiDir = path.resolve(process.cwd(), 'mp3-to-midi');
+  if (!fs.existsSync(midiDir)) {
+    fs.mkdirSync(midiDir);
+  }
+
+  await page.screenshot({ path: path.join(midiDir, 'converter-midi.png') });
 
   await browser.close();
 })();
